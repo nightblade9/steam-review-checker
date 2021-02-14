09dc98883f537533a3340b8db2fc9ae645f1925f
nightblade9 <nightbladecodes@gmail.com>
2021-02-14 01:18:38 -0500

    Fetching and printing reviews works.

--- main.py
+++ main.py
@@ -7,25 +7,38 @@ import urllib.request
 class Main:
     _CONFIG_JSON_FILENAME = "config.json"
     _LAST_SEEN_FILE = "last_seen_timestamp.txt"
+    _STEAM_APP_URL = "https://store.steampowered.com/app/" # append app_id
     _STEAM_BASE_URL = "https://store.steampowered.com/appreviews/{}?json=1"
     
 
     def main(self):
-        all_reviews = []
+        new_reviews = []
 
         config_json = self._read_config_json()
         app_ids = config_json["appIds"]
         last_timestamp = self._get_last_review_time()
 
         for app_id in app_ids:
-            # Call the API for each app and get reviews
-            url = Main._STEAM_BASE_URL.format(app_id)
-            response = urllib.request.urlopen(url).read()
-            json_response = json.loads(response.decode('utf-8'))
+            reviews = self._get_app_reviews(app_id)
+            for review in reviews:
+                if review["timestamp_created"] >= last_timestamp:
+                    new_reviews.append(review)
+                    review["app_id"] = app_id
 
-            if json_response["success"] != 1:
-                print("Error: failed to fetch API for app {}; response was: success={})".format(app_id, json_response["success"]))
-            
+        if len(new_reviews) == 0:
+            print("No new reviews, sorry.")
+            return
+        
+        print("***** {} new reviews! ****".format(len(new_reviews)))
+        previous_app_id = 0
+
+        for i in range(len(new_reviews)):
+            review = new_reviews[i]
+            if review["app_id"] != previous_app_id:
+                print("================ For app #{}: {}/{} ================".format(app_id, Main._STEAM_APP_URL, app_id))
+                previous_app_id = review["app_id"]
+            print("[Review #{}]: {}\n--------------------".format(i + 1, review["review"]))
+
         self._write_current_time()
 
     def _read_config_json(self):
@@ -50,5 +63,15 @@ class Main:
         with open(Main._LAST_SEEN_FILE, 'w') as file_handle:
             file_handle.write(str(time.time()))
         
+    def _get_app_reviews(self, app_id):
+        # Call the API for each app and get reviews
+        url = Main._STEAM_BASE_URL.format(app_id)
+        response = urllib.request.urlopen(url).read()
+        json_response = json.loads(response.decode('utf-8'))
+
+        if json_response["success"] != 1:
+            print("Error: failed to fetch API for app {}; response was: success={})".format(app_id, json_response["success"]))
+        
+        return json_response["reviews"]
 
 Main().main()
