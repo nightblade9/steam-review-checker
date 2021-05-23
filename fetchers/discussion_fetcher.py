59a1d58487b53227907c6ed8204b97d37498626b
nightblade9 <nightbladecodes@gmail.com>
2021-05-23 16:28:39 -0400

    Discussion parsing via lxml works.

--- fetchers/discussion_fetcher.py
+++ fetchers/discussion_fetcher.py
@@ -1,14 +1,15 @@
 #!/bin/python3
+from os import link
 from fetchers.steam_fetcher import SteamFetcher
 import re
 import urllib
+import lxml.html
 
 # Scrapes Steam's discussion forums to get you the latest greatest data.
 class DiscussionFetcher(SteamFetcher):
 
     _STEAM_DISCUSSIONS_URL = SteamFetcher._STEAM_COMMUNITY_URL + "/discussions/"
-    _NUMBER_OF_REPLIES_START = '<img src="https://community.akamai.steamstatic.com/public/images/skin_1/comment_quoteicon.png">'
-    _NUMBER_OF_REPLIES_STOP = '</div>'
+    _DISCUSSION_NODE_ROOT_XPATH = "//div[contains(@class, 'forum_topic ')]"
 
     def get_discussions(self):
         config_json = self._read_config_json()
@@ -20,20 +21,29 @@ class DiscussionFetcher(SteamFetcher):
             response = urllib.request.urlopen(url).read()
             raw_html = response.decode('utf-8')
 
-            # Repair HTML
+            # Repair HTML so we can use XPath
             raw_html = raw_html.replace('class="searchtext"', '')
+            
+            # Parse the output
+            root = lxml.html.fromstring(raw_html)
+            discussion_nodes = root.xpath(DiscussionFetcher._DISCUSSION_NODE_ROOT_XPATH)
 
-            reply_counts = self._get_reply_counts(raw_html)
-            print(str(reply_counts))
-    
-    def _get_reply_counts(self, raw_html):
-        to_return = []
-        for index in re.finditer(DiscussionFetcher._NUMBER_OF_REPLIES_START, raw_html):
-            start_index = index.span()[1]
-            stop_index = raw_html.index(DiscussionFetcher._NUMBER_OF_REPLIES_STOP, start_index)
-            print("Start={} stop={}".format(start_index, stop_index))
-            number_of_replies = raw_html[start_index:stop_index]
-            to_return.append(int(number_of_replies))
-            #print("{}, {} from {} to {}: {}".format(raw_html[index.span()[0]:index.span()[1]], number_of_replies, start_index, stop_index, number_of_replies))
-            print("RAw replies are: {}".format(number_of_replies))
-        return to_return
+            for i in range(len(discussion_nodes)):
+                node = discussion_nodes[i]
+                link_node = [a for a in node if a.tag == 'a'][0]
+                discussion_url = link_node.attrib["href"]
+
+                dissected_nodes = node.text_content().strip().split('\t\t\t\t')
+                # Dissected nodes is eight items, including some empty ones.
+                num_replies = dissected_nodes[0].strip()
+                discussion_date = dissected_nodes[2].strip()
+                title_and_author = dissected_nodes[7].split('\n')
+                title = title_and_author[0].strip()
+                author = title_and_author[-1].strip()
+                all_discussions.append({
+                    "title": title,
+                    "date": discussion_date,
+                    "author": author,
+                    "url": discussion_url,
+                    "num_replies": num_replies,
+                })
