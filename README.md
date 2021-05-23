5d9c79de253a26301d340fabfc211185de6fab86
nightblade9 <nightbladecodes@gmail.com>
2021-05-23 00:02:58 -0400

    Start v2 by gutting unnecessary code from v1

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Steam Review Checker
 
-Little script that pokes Steam for reviews and lets you know when there are new ones. (Keeps track of the last time it ran.)
-
-# Configuration
+Little script that pokes Steam for reviews/discussions.
 
 Open up `config.json` and fill in all the values. You can find the steam app ID from browsing the page for your game; the ID is the last numeric value, e.g. for Oneons (`https://store.steampowered.com/app/1342600/Oneons_Prisoners/`) the app ID is 1342600.
 
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #!/bin/python3
 import json
-import os
-import time
 import urllib.request
+import time
 
 class Main:
     _CONFIG_JSON_FILENAME = "config.json"
-    _LAST_SEEN_FILE = "last_seen_timestamp.txt"
+
+    # Not used, use it to get the app title
     _STEAM_APP_URL = "https://store.steampowered.com/app/" # append app_id
 
     # Sort by newest-first, up to the max (100 per page). See: https://partner.steamgames.com/doc/store/getreviews
@@ -14,48 +14,19 @@ class Main:
     
 
     def main(self):
-        new_reviews = []
-
         config_json = self._read_config_json()
         app_ids = config_json["appIds"]
-        last_timestamp = self._get_last_review_time()
-        self._write_current_time()
+        reviews = []
 
         for app_id in app_ids:
             reviews = self._get_app_reviews(app_id)
             for review in reviews:
-                if review["timestamp_created"] >= last_timestamp:
-                    new_reviews.append(review)
-                    review["app_id"] = app_id
-
-        if len(new_reviews) == 0:
-            elapsed_seconds = time.time() - last_timestamp
-            last_checked_days = round(elapsed_seconds / (60 * 60 * 24))
-            if last_checked_days == 0:
-                last_checked_days = "today"
-            else:
-                last_checked_days = "{} days ago".format(last_checked_days)
-            print("Last checked {}. No new reviews, sorry.".format(last_checked_days))
-            return
+                review["app_id"] = app_id
+                elapsed_seconds = time.time() - review["timestamp_created"]
+                days_ago = round(elapsed_seconds / (60 * 60 * 24))
+                review["days_ago"] = days_ago
         
-        print("***** {} new review(s)! ****".format(len(new_reviews)))
-        previous_app_id = 0
-
-        for i in range(len(new_reviews)):
-            review = new_reviews[i]
-            if review["app_id"] != previous_app_id:
-                print("================ For app #{}: {}/{} ================".format(app_id, Main._STEAM_APP_URL, app_id))
-                previous_app_id = review["app_id"]
-                review_number = 1
-            else:
-                review_number += 1
-            
-            # Pretty-print, e.g. 3 days ago
-            review_date = review["timestamp_created"]
-            elapsed_seconds = time.time() - review_date
-            days_ago = round(elapsed_seconds / (60 * 60 * 24))
-
-            print("[Review #{} ({} days ago)]: {}\n--------------------".format(review_number, days_ago, review["review"]))
+        print(reviews)
 
     def _read_config_json(self):
         config_json = ""
@@ -64,20 +35,6 @@ class Main:
         config = json.loads(config_json)
         return config
     
-    def _get_last_review_time(self):
-        # Check when's the last time we looked at reviews
-        last_timestamp = 0
-        if os.path.isfile(Main._LAST_SEEN_FILE):
-            with open(Main._LAST_SEEN_FILE, 'r') as file_handle:
-                last_timestamp = float(file_handle.read())
-
-        return last_timestamp
-
-    def _write_current_time(self):
-        # Write current time so we get newer reviews next time we run
-        with open(Main._LAST_SEEN_FILE, 'w') as file_handle:
-            file_handle.write(str(time.time()))
-        
     def _get_app_reviews(self, app_id):
         # Call the API for each app and get reviews
         url = Main._STEAM_BASE_URL.format(app_id)
