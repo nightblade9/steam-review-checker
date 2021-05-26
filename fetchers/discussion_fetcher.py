de07edf6cbdb78fc207885af3c62c5329c983c8c
nightblade9 <nightbladecodes@gmail.com>
2021-05-26 15:47:05 -0400

    Remaining discussion-fetcher tests + test data files. Refactor: extracted out parse_discussions method

--- fetchers/discussion_fetcher.py
+++ fetchers/discussion_fetcher.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 import datetime
+from re import L
 from fetchers.steam_fetcher import SteamFetcher
 import urllib
 import lxml.html
@@ -20,51 +21,59 @@ class DiscussionFetcher(SteamFetcher):
             url = DiscussionFetcher._STEAM_DISCUSSIONS_URL.format(app_id)
             response = urllib.request.urlopen(url).read()
             raw_html = response.decode('utf-8')
-
-            # Repair HTML so we can use XPath
-            raw_html = raw_html.replace('class="searchtext"', '')
+            discussions = _parse_discussions(raw_html, app_id, metadata[app_id]["game_name"])
             
-            # Parse the output
-            root = lxml.html.fromstring(raw_html)
-            discussion_nodes = root.xpath(DiscussionFetcher._DISCUSSION_NODE_ROOT_XPATH)
-
-            for i in range(len(discussion_nodes)):
-                node = discussion_nodes[i]
-
-                link_node = [a for a in node if a.tag == 'a'][0]
-                discussion_url = link_node.attrib["href"]
-
-                dissected_nodes = node.text_content().strip().split('\t\t\t\t')
-                # Dissected nodes is eight items, including some empty ones.
-                num_replies = dissected_nodes[0].strip()
-
-                title_and_author = dissected_nodes[7].split('\n')
-                title = title_and_author[0].strip()
-                author = title_and_author[-1].strip()
-
-                raw_date = dissected_nodes[2].strip()
-                discussion_date = parse_date(raw_date)
-
-                days_ago = (datetime.datetime.now() - discussion_date).days
-                date_formatted = discussion_date.strftime("%Y-%m-%d %H:%M")
-
-                all_discussions.append({
-                    "app_id": app_id,
-                    "title": title, # discussion title, not game name
-                    "date": date_formatted,
-                    "author": author,
-                    "url": discussion_url,
-                    "num_replies": num_replies,
-                    "days_ago": days_ago,
-                    "game_name": metadata[app_id]["game_name"]
-                })
+            for discussion in discussions:
+                all_discussions.append(discussion)
 
         # Sort by time descending, order of games isn't important
         all_discussions.sort(key=lambda x: x["date"], reverse=True)
 
         return all_discussions
 
-def parse_date(raw_date):
+def _parse_discussions(raw_html, app_id, game_name):
+    discussions = []
+    # Repair HTML so we can use XPath
+    raw_html = raw_html.replace('class="searchtext"', '')
+    
+    # Parse the output
+    root = lxml.html.fromstring(raw_html)
+    discussion_nodes = root.xpath(DiscussionFetcher._DISCUSSION_NODE_ROOT_XPATH)
+
+    for i in range(len(discussion_nodes)):
+        node = discussion_nodes[i]
+
+        link_node = [a for a in node if a.tag == 'a'][0]
+        discussion_url = link_node.attrib["href"]
+
+        dissected_nodes = node.text_content().strip().split('\t\t\t\t')
+        # Dissected nodes is eight items, including some empty ones.
+        num_replies = dissected_nodes[0].strip()
+
+        title_and_author = dissected_nodes[7].split('\n')
+        title = title_and_author[0].strip()
+        author = title_and_author[-1].strip()
+
+        raw_date = dissected_nodes[2].strip()
+        discussion_date = _parse_date(raw_date)
+
+        days_ago = (datetime.datetime.now() - discussion_date).days
+        date_formatted = discussion_date.strftime("%Y-%m-%d %H:%M")
+
+        discussions.append({
+            "app_id": app_id,
+            "title": title, # discussion title, not game name
+            "date": date_formatted,
+            "author": author,
+            "url": discussion_url,
+            "num_replies": num_replies,
+            "days_ago": days_ago,
+            "game_name": game_name
+        })
+    
+    return discussions
+
+def _parse_date(raw_date):
     # Ah, Steam, ah. Discussion dates can have a variety of interesting, painful formats.
     # 1) The most stable are dates older than a year: we get a full year, month, and day (e.g. April 29, 2019).
     # 2) Reviews this year, have no year attached to them (e.g. May 20).
--- tests/fetchers/test_discussion_fetcher.py
+++ tests/fetchers/test_discussion_fetcher.py
@@ -1,4 +1,5 @@
 import datetime
+import os
 import unittest
 
 from fetchers import discussion_fetcher
@@ -8,28 +9,28 @@ class TestDicussionFetcher(unittest.TestCase):
 
     def test_parse_date_converts_empty_string_to_now(self):
         for test_case in ['', '          ']:
-            actual = discussion_fetcher.parse_date(test_case)
+            actual = discussion_fetcher._parse_date(test_case)
             self.assertAlmostEqual(actual, datetime.datetime.now(), datetime.timedelta(seconds=1))
     
     def test_parse_date_converts_just_now_to_now(self):
         for test_case in ["just NOW", "Just now", "JuSt NoW"]:
-            actual = discussion_fetcher.parse_date(test_case)
+            actual = discussion_fetcher._parse_date(test_case)
             self.assertAlmostEqual(actual, datetime.datetime.now(), datetime.timedelta(seconds=1))
 
     def test_parse_date_converts_minutes_ago_to_now_with_delta(self):
         for test_case in [8, 38, 1, 59]:
-            actual = discussion_fetcher.parse_date("{} minutes ago".format(test_case))
+            actual = discussion_fetcher._parse_date("{} minutes ago".format(test_case))
             self.assertAlmostEqual(actual, datetime.datetime.now() + datetime.timedelta(minutes=test_case), datetime.timedelta(seconds=1))
     
     def test_parse_date_converts_hours_ago_to_now_with_delta(self):
         for test_case in [17, 6, 1, 23]:
-            actual = discussion_fetcher.parse_date("{} hours ago".format(test_case))
+            actual = discussion_fetcher._parse_date("{} hours ago".format(test_case))
             self.assertAlmostEqual(actual, datetime.datetime.now() + datetime.timedelta(hours=test_case), datetime.timedelta(seconds=1))
 
     def test_parse_date_converts_yearless_dates_to_current_year(self):
         for test_case in ["29 May", "1 Jan", "31 Dec", "28 Feb", "17 Aug"]:
             expected = datetime.datetime.strptime(test_case, "%d %b")
-            actual = discussion_fetcher.parse_date("{} @ 1:23PM".format(test_case))
+            actual = discussion_fetcher._parse_date("{} @ 1:23PM".format(test_case))
             self.assertEqual(actual.year, datetime.datetime.now().year)
             self.assertEqual(actual.month, expected.month),
             self.assertEqual(actual.day, expected.day)
@@ -37,7 +38,51 @@ class TestDicussionFetcher(unittest.TestCase):
     def test_parse_date_parses_date_with_year(self):
         for test_case in ["29 Jun, 2021", "1 Jan, 2002", "31 Dec, 1976", "16 Mar, 2015", "4 Oct, 2011"]:
             expected = datetime.datetime.strptime(test_case, "%d %b, %Y")
-            actual = discussion_fetcher.parse_date("{} @ 11:59pm".format(test_case))
+            actual = discussion_fetcher._parse_date("{} @ 11:59pm".format(test_case))
             self.assertEqual(actual.year, expected.year)
             self.assertEqual(actual.month, expected.month)
             self.assertEqual(actual.day, expected.day)
+    
+    # For Oneons: detailed parsing since it's just one discussion, check ALL fields.
+    def test_parse_discussions_can_parse_oneons_discussions(self):
+        raw_html = ""
+        app_id = 1342600
+        
+        with open(os.path.join("tests", "test_data", "steam_discussions", "{}.html".format(app_id)), 'r', encoding="utf-8") as file_handle:
+            raw_html = file_handle.read()
+        
+        actual = discussion_fetcher._parse_discussions(raw_html, app_id, "Oneons")
+
+        self.assertEqual(1, len(actual))
+        discussion = actual[0]
+        self.assertEqual(app_id, discussion["app_id"])
+        self.assertEqual("A few Suggestions", discussion["title"])
+        self.assertEqual("Benjo Bobbington", discussion["author"])
+        self.assertEqual("https://steamcommunity.com/app/1342600/discussions/0/2646378342121880438/", discussion["url"])
+        self.assertEqual(2, int(discussion["num_replies"]))
+        self.assertEqual("Oneons", discussion["game_name"])
+    
+    # For other games: review count is sufficient. This covers: non-English reviews, free-key-redeemed reviews, etc.
+    def test_parse_discussions_can_parse_up_to_15_discussions(self):
+        test_cases = [
+            {
+                "app_id": 1000640,
+                "expected": 11
+            },
+            {
+                "app_id": 1512860,
+                "expected": 12
+            }
+        ]
+
+        for data in test_cases:
+            raw_html = ""
+            app_id = data["app_id"]
+            expected = data["expected"]
+            
+            with open(os.path.join("tests", "test_data", "steam_discussions", "{}.html".format(app_id)), 'r', encoding="utf-8") as file_handle:
+                raw_html = file_handle.read()
+            
+            actual = discussion_fetcher._parse_discussions(raw_html, app_id, "Title goes here")
+
+            self.assertEqual(expected, len(actual)) # maxes out at 15 discussions
