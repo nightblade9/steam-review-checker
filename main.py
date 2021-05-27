a5a0d75ad73ed59c48cacd2a8485abb2fbadf9bd
Bilal Akil <mail@bilalakil.me>
2021-05-28 01:19:09 +1000

    Simplified web

--- main.py
+++ main.py
@@ -7,42 +7,34 @@ import shutil
 import subprocess
 import time
 
+WEB_DIR = os.path.join(os.path.dirname(__file__), "web")
+WEB_DATA_DIR = os.path.join(WEB_DIR, "data")
+
 class Main:
     def main(self):
         # Start web server.  This nukes build/data, so we have to wait for it to finish.
-        self._build_web_app()
-
         self._ensure_output_directory_exists()
         self._fetch_all_data()
 
-        # Copy data to public/data. Makes local development easier
-        if os.path.exists(os.path.join("web", "public", "data")):
-            shutil.rmtree(os.path.join("web", "public", "data"))
-        shutil.copytree(os.path.join("web", "build", "data"), os.path.join("web", "public", "data"))
-
         # Start web server
-        subprocess.Popen(["python", "-m", "http.server"], cwd=os.path.join("web", "build"), shell=True)
-
-    def _build_web_app(self):
-        # Build the self-hostable version of the app
-        subprocess.run(["npm", "run", "build"], cwd="web", shell=True)
+        subprocess.Popen(["python", "-m", "http.server"], cwd=WEB_DIR, shell=True)
 
     def _fetch_all_data(self):
         start_time = time.time()
 
         metadata = GameFetcher().get_game_metadata() # metadata like titles
         all_metadata_json = json.dumps(metadata)
-        with open(os.path.join("web", "build", "data", "metadata.json"), "w") as file_handle:
+        with open(os.path.join(WEB_DATA_DIR, "metadata.json"), "w") as file_handle:
             file_handle.write(all_metadata_json)
 
         all_reviews = ReviewFetcher().get_reviews(metadata)
         all_reviews_json = json.dumps(all_reviews)
-        with open(os.path.join("web", "build", "data", "reviews.json"), "w") as file_handle:
+        with open(os.path.join(WEB_DATA_DIR, "reviews.json"), "w") as file_handle:
             file_handle.write(all_reviews_json)
 
         all_discussions = DiscussionFetcher().get_discussions(metadata)
         all_discussions_json = json.dumps(all_discussions)
-        with open(os.path.join("web", "build", "data", "discussions.json"), "w") as file_handle:
+        with open(os.path.join(WEB_DATA_DIR, "discussions.json"), "w") as file_handle:
             file_handle.write(all_discussions_json)
 
         stop_time = time.time()
@@ -50,10 +42,7 @@ class Main:
         print("*** Fetched data in {0:g}s ***".format(elapsed_time))
     
     def _ensure_output_directory_exists(self):
-        if not os.path.isdir(os.path.join("web", "build")):
-            os.mkdir(os.path.join("web", "build"))
-
-        if not os.path.isdir(os.path.join("web", "build", "data")):
-            os.mkdir(os.path.join("web", "build", "data"))
+        if not os.path.isdir(WEB_DATA_DIR):
+            os.mkdir(WEB_DATA_DIR)
 
 Main().main()
