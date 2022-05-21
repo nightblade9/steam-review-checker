aadf315411b2162df5332430ecf89e1a5a9f61e9
Bilal Akil <mail@bilalakil.me>
2022-05-22 00:17:10 +1000

    Stylisation and consistency changes for JS

--- web/main.js
+++ web/main.js
@@ -6,8 +6,8 @@
     event.stopPropagation();
 
     { // Discussions
-      const discussionCtnr = document.querySelector("#discussion-container");
-      const discussionStars = discussionCtnr.querySelectorAll(".unread-star");
+      const discussionContainer = document.querySelector("#discussion-container");
+      const discussionStars = discussionContainer.querySelectorAll(".unread-star");
       for (const discussionStar of discussionStars) {
         const key = discussionStar.getAttribute("data-key");
         readDiscussions[key] = true;
@@ -18,8 +18,8 @@
     }
 
     { // Reviews
-      const reviewCtnr = document.querySelector("#review-container");
-      const reviewStars = reviewCtnr.querySelectorAll(".unread-star");
+      const reviewContainer = document.querySelector("#review-container");
+      const reviewStars = reviewContainer.querySelectorAll(".unread-star");
       for (const reviewStar of reviewStars) {
         const key = reviewStar.getAttribute("data-key");
         readReviews[key] = true;
@@ -50,15 +50,14 @@
 
   const getLocalStorageJSONObject = (key) => {
     const jsonStr = localStorage.getItem(key);
-    if (jsonStr === null)
+    if (jsonStr === null) {
       return null;
+    }
 
-    try
-    {
+    try {
       return JSON.parse(jsonStr);
     }
-    catch
-    {
+    catch {
       localStorage.removeItem(key);
       return null;
     }
@@ -68,7 +67,7 @@
     localStorage.setItem(key, JSON.stringify(obj));
 
   const getData = async (file) => {
-    var headers = new Headers();
+    const headers = new Headers();
     headers.append("pragma", "no-cache");
     headers.append("cache-control", "no-cache");
 
@@ -77,17 +76,19 @@
       headers: headers
     });
 
-    var toReturn = await res.json();
+    const toReturn = await res.json();
     return toReturn;
   };
 
   const populateState = async () => {
     readReviews = getLocalStorageJSONObject("readReviews");
-    if (readReviews === null)
+    if (readReviews === null) {
       readReviews = new Map();
+    }
     readDiscussions = getLocalStorageJSONObject("readDiscussions");
-    if (readDiscussions === null)
+    if (readDiscussions === null) {
       readDiscussions = new Map();
+    }
 
     metadata = await getData("metadata.json");
     reviews = await getData("reviews.json");
@@ -95,13 +96,13 @@
   };
 
   const render = () => {
-    const ctnr = document.querySelector("#container");
-    ctnr.innerHTML = "";
+    const container = document.querySelector("#container");
+    container.innerHTML = "";
 
-    renderHeader(ctnr);
-    renderDiscussions(ctnr);
-    renderReviews(ctnr);
-    hideStarsForReadItems(ctnr);
+    renderHeader(container);
+    renderDiscussions(container);
+    renderReviews(container);
+    hideStarsForReadItems(container);
   };
 
   // Magically replaces all template values with the value from the dictionary.
@@ -112,11 +113,11 @@
   const getTime = (daysAgo) => 
     daysAgo >= 365 ? Math.floor(daysAgo / 365) + " years" : daysAgo + " days";
 
-  const renderHeader = (ctnr) => {
+  const renderHeader = (container) => {
     { // Container
       const template = document.querySelector("#header-container-template").innerHTML;
       const html = template;
-      ctnr.insertAdjacentHTML("beforeend", html);
+      container.insertAdjacentHTML("beforeend", html);
     }
 
     {
@@ -125,12 +126,10 @@
       // amend metadata to add number of reviews to each game
       const reviewsPerGame = {};
 
-      Object.entries(reviews).forEach(review =>
-      {
-        var reviewData = review[1];
-        var appId = reviewData.app_id;
-        if (!(appId in reviewsPerGame))
-        {
+      Object.entries(reviews).forEach(review => {
+        const reviewData = review[1];
+        const appId = reviewData.app_id;
+        if (!(appId in reviewsPerGame)) {
           reviewsPerGame[appId] = 0;
         }
 
@@ -140,12 +139,10 @@
       // amend metadata to add number of discussions to each game
       const discussionsPerGame = {};
 
-      Object.entries(discussions).forEach(discussion =>
-      {
-        var discussionData = discussion[1];
-        var appId = discussionData.app_id;
-        if (!(appId in discussionsPerGame))
-        {
+      Object.entries(discussions).forEach(discussion => {
+        const discussionData = discussion[1];
+        const appId = discussionData.app_id;
+        if (!(appId in discussionsPerGame)) {
           discussionsPerGame[appId] = 0;
         }
 
@@ -155,15 +152,15 @@
 
       // Reformulate into expected data structure
       const data = [];
-      Object.entries(metadata).forEach(([appId, gameMetadata]) => {
+      Object.entries(metadata).forEach(([appId, gameMetadata]) =>
         data[appId] = {
           "reviews": reviewsPerGame[appId] || 0, // 0 not undefined if no reviews,
           "discussions": discussionsPerGame[appId] || 0,
           "gameName": gameMetadata["game_name"]
-        };
-      });
+        }
+      );
 
-      const headerContainer = ctnr.querySelector("#header-container");
+      const headerContainer = container.querySelector("#header-container");
       const template = document.querySelector("#header-template").innerHTML;
       data.forEach(_ => {
         const html = applyDataToTemplate(_, template);
@@ -172,22 +169,22 @@
     }
   };
 
-  const renderDiscussions = (ctnr) => {
+  const renderDiscussions = (container) => {
     { // Container
       const template = document.querySelector("#discussion-container-template").innerHTML;
       
-      var totalPosts = discussions.length;
-      discussions.forEach(d => { totalPosts += d.num_replies; });
+      let totalPosts = discussions.length;
+      discussions.forEach(d => totalPosts += d.num_replies);
       const data = { numDiscussions: discussions.length, totalPosts: totalPosts };
       const html = applyDataToTemplate(data, template);
-      ctnr.insertAdjacentHTML("beforeend", html);
+      container.insertAdjacentHTML("beforeend", html);
     }
 
     { // Individual discussions
-      const discussionCtnr = ctnr.querySelector("#discussion-container");
+      const discussionContainer = container.querySelector("#discussion-container");
       const template = document.querySelector("#discussion-template").innerHTML;
       discussions.forEach(_ => {
-        var data = {
+        const data = {
           game: metadata[_.app_id].game_name,
           time: getTime(_.days_ago),
           numReplies: _.num_replies + 1, // +1 to add the thread itself, don't want to see a post with "(0 posts)"
@@ -196,26 +193,26 @@
           appId: _.appId,
           subforum: _.subforum
         };
-        Object.assign(data, _) // merge all fields from the JSON object
+        Object.assign(data, _); // merge all fields from the JSON object
         const html = applyDataToTemplate(data, template);
-        discussionCtnr.insertAdjacentHTML("beforeend", html);
+        discussionContainer.insertAdjacentHTML("beforeend", html);
       });
     }
   };
 
-  const renderReviews = (ctnr) => {
+  const renderReviews = (container) => {
     { // Container
       const template = document.querySelector("#review-container-template").innerHTML;
       const data = { numReviews: reviews.length };
       const html = applyDataToTemplate(data, template);
-      ctnr.insertAdjacentHTML("beforeend", html);
+      container.insertAdjacentHTML("beforeend", html);
     }
 
     { // Individual reviews
-      const reviewCtnr = ctnr.querySelector("#review-container");
+      const reviewContainer = container.querySelector("#review-container");
       const template = document.querySelector("#review-template").innerHTML;
       reviews.forEach(_ => {
-        var data = {
+        const data = {
           score: _.voted_up ? "positive" : "negative",
           posOrNeg: _.voted_up ? "Positive" : "Negative",
           game: metadata[_.app_id].game_name,
@@ -227,29 +224,31 @@
         };
         Object.assign(data, _) // merge all fields from the JSON object
         const html = applyDataToTemplate(data, template);
-        reviewCtnr.insertAdjacentHTML("beforeend", html);
+        reviewContainer.insertAdjacentHTML("beforeend", html);
       });
     }
   };
 
-  const hideStarsForReadItems = (ctnr) => {
+  const hideStarsForReadItems = (container) => {
     { // Discussions
-      const discussionCtnr = document.querySelector("#discussion-container");
-      const discussionStars = discussionCtnr.querySelectorAll(".unread-star");
+      const discussionContainer = container.querySelector("#discussion-container");
+      const discussionStars = discussionContainer.querySelectorAll(".unread-star");
       for (const discussionStar of discussionStars) {
         const key = discussionStar.getAttribute("data-key");
-        if (key in readDiscussions)
+        if (key in readDiscussions) {
           discussionStar.classList.add("hidden");
+        }
       }
     }
 
     { // Reviews
-      const reviewCtnr = document.querySelector("#review-container");
-      const reviewStars = reviewCtnr.querySelectorAll(".unread-star");
+      const reviewContainer = container.querySelector("#review-container");
+      const reviewStars = reviewContainer.querySelectorAll(".unread-star");
       for (const reviewStar of reviewStars) {
         const key = reviewStar.getAttribute("data-key");
-        if (key in readReviews)
+        if (key in readReviews) {
           reviewStar.classList.add("hidden");
+        }
       }
     }
   }
